48e842efcb100c6bebdf6ee095a6375646f94e10
colgan <colgantylermitchell@gmail.com>
2022-09-23 14:41:08 -0500

    Update 1.0
    + register HWID
    + login w/ HWID
    + faster sql query
    + added testing to after auth menu

---
+++ menu.py
@@ -0,0 +1,35 @@
+import os
+import random
+import time
+import requests
+class title():
+    letter ='SDK2LFSKAEPOEUYRBZJFKELAXA3411LFK'#line:9
+    os .system ("title "+"".join (random.choices (letter ,k =17 )))
+
+def main():
+    from time import sleep
+    from tqdm import tqdm
+    for i in tqdm(range(7)):
+        sleep(0.5)
+    import db
+    os.system('cls')
+class mai():
+    ans =True
+    while ans :
+        print(" ____________________ "
+        "\n     craigs thing"
+        " \n ____________________")
+        print ("""        
+        1.Login
+        2.Register
+        3.Exit
+        """ "")
+        ans =input ("SELECT: \n")
+        if ans =="1":
+            os.system('cls')
+            main()
+        if ans =="2":
+            import gethw
+
+        if ans =="3":
+            sys.exit()
---
+++ prgx.py
@@ -0,0 +1,24 @@
+import subprocess
+import pymysql as MySQLdb
+import time
+import os
+import random
+
+letter ='SDK2LFSKAEPOEUYRBZJFKELAXA3411LFK'#line:9
+os .system ("title "+"".join (random .choices (letter ,k =17 )))
+
+os.system('cls')
+ans = True
+if ans == True:
+    print("____________________ "
+          "\n     craigs thing"
+          " \n ____________________")
+    print(""" 
+    1.TESTING
+    2.Exit """"")
+    ans = input("SELECT: \n")
+    os.system('cls')
+
+if ans == "2":
+    os.system('cls')
+    raise SystemExit
